555970f8dc0df606b14def42396cb56cda2b63cb
biattasawka <biattasawka@gmail.com>
2026-07-02 11:41:55 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,12 +1,23 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-op = input("Enter operation (+, -, *, /): ")
-if op == "+":
-	result = num1 + num2
-if op == "-":
-	result = num1 - num2
-if op == "*":
-	result = num1 * num2
-if op == "/":
-	result = num1 / num2
-print(result)
+while True:
+	num1 = float(input("Enter first number: "))
+	num2 = float(input("Enter second number: "))
+	op = input("Enter operation (+, -, *, /): ")
+	if op == "exit":
+		print("Goodbye!")
+		break
+	if op == "+":
+		result = num1 + num2
+	elif op == "-":
+		result = num1 - num2
+	elif op == "*":
+		result = num1 * num2
+	elif op == "/":
+		if num2 != 0:
+			result = num1 / num2
+		else:
+			print("Cannot divide by zero")
+			continue
+	else:
+		print("Invalid operation")
+		continue
+	print("Result:",result)
